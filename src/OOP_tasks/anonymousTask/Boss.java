f05b6a63355d1b9c3b89a78da75ddfa8e60955a7
yern1r <yernaryelemess@gmail.com>
2022-12-28 13:57:24 +0600

    Udemy course java

--- src/OOP_tasks/anonymousTask/Boss.java
+++ src/OOP_tasks/anonymousTask/Boss.java
@@ -4,7 +4,8 @@ public class Boss {
     public void force(Counter counter, int i){
         String report = counter.report(i);
         //giving value to variable report => instance of Counter interface, counter
-        //then calling method report with parameter int month which will initialized with i
+        //then calling method report with parameter int month
+        // which will initialized with i , it is possible because they are same data type
         System.out.println(report);
     }
 
